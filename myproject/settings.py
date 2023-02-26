ee0052940657f64c7a2d70780212097b6f09bc37
anandubs1409 <anandu@neuralgarage.com>
2023-02-26 11:09:14 +0530

    requirements & db settings changed

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,15 +83,27 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 #    }
 #}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'openbrights',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME':'crm',
+            'USER':'admin',
+            'PASSWORD':'Admin.com#123.com#',
+            'HOST':'bright.csx7kxzz8bod.ap-south-1.rds.amazonaws.com',
+            'PORT':'3306',
+            'OPTIONS': {
+                'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+            }
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
